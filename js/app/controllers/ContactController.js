3392e919f1997c0d8f15556c8901e03caf7dc0b7
jchu4483 <jchu4483@gmail.com>
2016-12-17 20:24:36 -0500

    Done.

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,5 +1,6 @@
 function ContactController() {
-    this.contacts = [{
+    var vm = this;
+    vm.contacts = [{
         name: 'Bob',
         phone: '0123458690'
     },{
@@ -10,8 +11,15 @@ function ContactController() {
         phone: '0684059433'
     }];
 
-    this.removeContact = function (index) {
-        this.contacts.splice(index, 1);
+    vm.addContact = function() {
+      vm.contacts.push({
+        name: vm.name,
+        phone: vm.phone
+      })
+    };
+
+    vm.removeContact = function (index) {
+        vm.contacts.splice(index, 1);
     }
 }
 
